981e4a0265e8ff6250d72a865c2b1049b30b2562
VladimirPigarev <109297010+VladimirPigarev@users.noreply.github.com>
2022-12-03 20:59:04 +0300

    написал код для решения задачи номер 2

--- z2_dz_5.py
+++ z2_dz_5.py
@@ -6,17 +6,47 @@
 # Сколько конфет нужно взять первому игроку, чтобы забрать все конфеты у своего конкурента?
 # a) Добавьте игру против бота
 # b) Подумайте как наделить бота ""интеллектом""
-import random
-kofety = 2021
-igrok1 = input("Игрок № 1, представтесь! ")
-igrok2 = input("Игрок № 2, представтесь! ")
-print(f'Привет {igrok1} и {igrok2}')
-print("Сейчас с помощью жеребьевки определим кто ходит первым")
-step1 = random.randint(1,6)
-step2 = random.randint(1,6)
-print(f'{igrok1} выкидывает {step1}, {igrok2} выкидывает {step2}')
-if step1 > step2: 
-    print(f"Первым ходит {igrok1}") 
-elif step1 < step2:
-    print(f'Первым ходит {igrok2}')
-    
+
+from random import randint
+
+igrok1 = input("Игрок № 1, представтесь: ")
+igrok2 = input("Игрок № 2, представтесь: ")
+konfety = int(input("количество конфет на столе: "))
+
+marker = randint(0,2) 
+if marker:
+    print(f"Первый ходит {igrok1}")
+else:
+    print(f"Первый ходит {igrok2}")
+
+count1 = 0 
+count2 = 0
+
+def input_konfety(name): # функция для ввода количества конфет пользователем
+    n = int(input(f"{name}, введите количество конфет, которое возьмете от 1 до 28: "))
+    while n < 1 or n > 28:
+        n = int(input(f"{name}, введите корректное количество конфет: "))
+    return n
+
+
+def step(name, k, count, konfety): # функция подсчета остатка
+    print(f"Ходил {name}, он взял {k}, теперь у него {count}. Осталось на столе {konfety} конфет.")
+
+while konfety > 28:
+    if marker:
+        k = input_konfety(igrok1)
+        count1 += k
+        konfety -= k
+        marker = False
+        step(igrok1, k, count1, konfety)
+    else:
+        k = input_konfety(igrok2)
+        count2 += k
+        konfety -= k
+        marker = True
+        step(igrok2, k, count2, konfety)
+
+if marker:
+    print(f"Выиграл {igrok1}")
+else:
+    print(f"Выиграл {igrok2}")
