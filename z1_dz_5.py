9a7e6f2ef8258387e28adfb0f725eb65e409f9bc
VladimirPigarev <109297010+VladimirPigarev@users.noreply.github.com>
2022-12-03 14:50:12 +0300

    Немного подправил код

--- z1_dz_5.py
+++ z1_dz_5.py
@@ -1,15 +1,15 @@
 # Напишите программу, удаляющую из текста все слова, содержащие ""абв"".
-input_text = str(input("Введите текст содержащий в словах фрагмент (абв), например: Привет, абвиатура содержит абв? : ")) 
+input_text = 'Привет, абвиатура содержит абв?'
 print(input_text)
-res_text = input_text.split(' ')
-fragment = 'абв'
-new_mas = []
+res_text = input_text.split(' ') # разбиваем строку на слова
+fragment = 'абв' # искомый фрагмент в слове
+new_mas = [] 
 for i in res_text:
-    if fragment not in i:
-        new_mas.append(i)
+    if fragment not in i: # условие, при котором проверяется, содержится ли фрагмент в слове.
+        new_mas.append(i) # создаем список из слов, не содержащих заданный фрагмент
 print(new_mas)
-itog = ' '.join(new_mas)
+itog = ' '.join(new_mas) # преобразуем список в строку опять
 print(itog)
-my_file = open('file.txt', 'w+', encoding="utf-8")
-my_file.write(itog)
+my_file = open('file.txt', 'w+', encoding="utf-8") # создаем файл
+my_file.write(itog) # записываем в файл итог прошлых действий
 my_file.close()
